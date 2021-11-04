b0ad890f8223bc61ddabab04d4ab900ba7578dbb
osvaldo.ferreira <osvaldo.ferreira@havan.com.br>
2021-11-03 23:46:08 -0300

    Cria funcoes para calcular pontuacao

--- src/blackjack_v2/game.clj
+++ src/blackjack_v2/game.clj
@@ -7,13 +7,33 @@
   (inc (rand-int 13))
   )
 
+(defn JQK->10
+  [card]
+  (if (> card 10) 10 card))
+
+(defn A->11
+  [card]
+  (if (= card 1) 11 card))
+
+(defn points-cards [cards]
+  (let [card-without-JQK (map JQK->10 cards)
+        card-with-A11   (map A->11 card-without-JQK)
+        points-with-A-11  (reduce + card-with-A11)
+        points-with-A1    (reduce + card-without-JQK)]
+   (if (> points-with-A-11 21) points-with-A1 points-with-A-11)
+    )
+  )
+
+
 (defn player
-  [player-name]
-  (let [card1 (new-card)
-        card2 (new-card)]
+  [player-name] (let [card1 (new-card)
+        card2 (new-card)
+        cards [card1 card2]
+        points (points-cards cards)]
     {
      :player-name player-name
-     :cards       [card1 card2]
+     :cards       cards
+     :points      points
      }
     )
   )
